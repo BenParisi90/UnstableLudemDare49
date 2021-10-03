d05c89f1e9c2f2f2d012b2d89050d18bc8dd1305
BenParisi <BenParisi90@gmail.com>
2021-10-03 13:34:25 -0700

    barn and background video

--- UnstableLudemDare49/Assets/Scripts/BackgroundVideo.cs
+++ UnstableLudemDare49/Assets/Scripts/BackgroundVideo.cs
@@ -5,10 +5,11 @@ using UnityEngine.Video;
 
 public class BackgroundVideo : MonoBehaviour
 {
+    [SerializeField] string videoName;
     [SerializeField] VideoPlayer videoPlayer;
     // Start is called before the first frame update
     void Start()
     {
-        videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,"SmallerHorseSkies.mp4"); 
+        videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,videoName); 
     }
 }
